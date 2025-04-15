523b82f0e20775cb7fcd7cf1c5f597c673462c83
SafyaAbouelfadel <safia.abf@gmail.com>
2025-04-15 02:18:10 +0000

    block

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  // eslint-disable-next-line no-empty
+  if (trueOrFalse) {
+  }
+
+  return [task, task2];
+}
