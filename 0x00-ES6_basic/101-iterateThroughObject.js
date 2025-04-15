722dfad13098e1b971c4739c5c65fba4df86fb84
SafyaAbouelfadel <safia.abf@gmail.com>
2025-04-15 04:31:10 +0000

    iterate through obj

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
   const employeesArray = [...reportWithIterator];
 
-  return employeesArray.join(` | `);
+  return employeesArray.join(' | ');
 }
